2f67165a5f2adfadc642066df7ce0d5777ef3645
RoyMcCrain <matsuda.r@kanamekey.com>
2023-08-08 10:55:46 +0900

    Update

--- nvim/plugins/fern.lua
+++ nvim/plugins/fern.lua
@@ -1,11 +1,9 @@
 vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>Fern . -reveal=%<CR>', {noremap = true, silent = true})
 
-local function init_fern()
+_G.init_fern = function()
   vim.api.nvim_buf_set_keymap(0, 'n', 'V', '<Plug>(fern-action-open:vsplit)', {})
   vim.api.nvim_buf_set_keymap(0, 'n', 'S', '<Plug>(fern-action-open:split)', {})
 end
-
-_G.init_fern = init_fern
 
 vim.api.nvim_exec([[
   augroup fern_custom
