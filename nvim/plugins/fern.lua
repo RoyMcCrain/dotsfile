d130f0c939e946aa079596beada55a1ed9ed3026
RoyMcCrain <matsuda.r@kanamekey.com>
2023-11-13 23:04:20 +0900

    Update

--- nvim/plugins/fern.lua
+++ nvim/plugins/fern.lua
@@ -1,8 +1,14 @@
 vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>Fern . -reveal=%<CR>', {noremap = true, silent = true})
+vim.g.fern_disable_default_mappings = 1
 
 _G.init_fern = function()
+  vim.api.nvim_buf_set_keymap(0, 'n', '<CR>','<Plug>(fern-action-open-or-expand)', {})
+  vim.api.nvim_buf_set_keymap(0, 'n', '<BS>', '<Plug>(fern-action-collapse)', {})
+  vim.api.nvim_buf_set_keymap(0, 'n', '<Left>', '<Plug>(fern-action-collapse)', {})
+  vim.api.nvim_buf_set_keymap(0, 'n', '<Right>', '<Plug>(fern-action-expand)', {})
   vim.api.nvim_buf_set_keymap(0, 'n', 'V', '<Plug>(fern-action-open:vsplit)', {})
   vim.api.nvim_buf_set_keymap(0, 'n', 'S', '<Plug>(fern-action-open:split)', {})
+  vim.api.nvim_buf_set_keymap(0, 'n', 'D', '<Plug>(fern-action-remove)', {})
 end
 
 vim.api.nvim_exec([[
