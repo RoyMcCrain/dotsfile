0881ba1a8bf18a79808a87afe8c1e895c29e294b
RoyMcCrain <me@rrrr.dev>
2023-12-11 13:01:11 +0900

    Removed copilot.vim config

--- nvim/plugins/copilot.lua
+++
@@ -1,7 +0,0 @@
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_no_maps = true
-vim.api.nvim_set_keymap('i', '<C-t><C-t>', 'copilot#Accept(\'<CR>\')', { expr = true, silent = true, noremap = true })
-vim.api.nvim_set_keymap('i', '<C-t><C-n>', '<Plug>(copilot-next)', { silent = true, noremap = true })
-vim.api.nvim_set_keymap('i', '<C-t><C-p>', '<Plug>(copilot-previous)', { silent = true, noremap = true })
-vim.api.nvim_set_keymap('i', '<C-t><C-d>', '<Plug>(copilot-dismiss)', { silent = true, noremap = true })
-vim.api.nvim_set_keymap('i', '<C-t><C-s>', '<Plug>(copilot-suggest)', { silent = true, noremap = true })
