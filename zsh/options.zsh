281bc0c95ef1fb6adc0f7a7edd69a1db21598614
RoyMcCrain <me@rrrr.dev>
2024-04-27 01:51:15 +0900

    Fix

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -56,8 +56,8 @@ zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' stagedstr '%F{green}!'
 zstyle ':vcs_info:*' unstagedstr '%F{yellow}+'
 
-zstyle ':vcs_info:*' formats '%F{green}%s:%b%c%u%f'
-zstyle ':vcs_info:*' actionformats '%F{red}%s:%b|%a%c%u%f'
+zstyle ':vcs_info:*' formats '%c%u%F{green}%s:%b%f'
+zstyle ':vcs_info:*' actionformats '%%c%uF{red}%s:%b|%a%f'
 
 function _update_vcs_info_msg() {
 		LANG=en_US.UTF-8 vcs_info
