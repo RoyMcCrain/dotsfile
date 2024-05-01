d53363d39f7dfbbc3e5e20b65aa083ea5f258395
RoyMcCrain <me@rrrr.dev>
2024-05-01 22:47:22 +0900

    Add -f option to abbr command

--- zsh/abbreviations.zsh
+++ zsh/abbreviations.zsh
@@ -1,44 +1,44 @@
-abbr --quiet js='jobs'
+abbr -f --quiet js='jobs'
 # fg %1 でそのjobsの復帰
 
-if command -v eza >/dev/null 2>&1; then
-	abbr --quiet ls='eza -TF -L=1 --icons'
-	abbr --quiet la='eza -aTF -L=1 --icons'
-	abbr --quiet ll='eza -lTF -L=1 --icons'
-	abbr --quiet lla='eza -alTF -L=1 --icons'
+if type eza >/dev/null 2>&1; then
+	abbr -f --quiet ls='eza -TF -L=1 --icons'
+	abbr -f --quiet la='eza -aTF -L=1 --icons'
+	abbr -f --quiet ll='eza -lTF -L=1 --icons'
+	abbr -f --quiet lla='eza -alTF -L=1 --icons'
 else
-	abbr --quiet la='ls -a'
-	abbr --quiet ll='ls -lh'
-	abbr --quiet lla='ls -lha'
+	abbr -f --quiet la='ls -a'
+	abbr -f --quiet ll='ls -lh'
+	abbr -f --quiet lla='ls -lha'
 fi
 
-abbr --quiet rm='rm -ri'
-abbr --quiet rmf='rm -rf'
-abbr --quiet cp='cp -i'
-abbr --quiet mv='mv -i'
+abbr -f --quiet rm='rm -ri'
+abbr -f --quiet rmf='rm -rf'
+abbr -f --quiet cp='cp -i'
+abbr -f --quiet mv='mv -i'
 
-abbr --quiet mkdir='mkdir -p'
+abbr -f --quiet mkdir='mkdir -p'
 
-abbr --quiet a="nvim"
-abbr --quiet code="code -n ."
-abbr --quiet g="git"
+abbr -f --quiet a="nvim"
+abbr -f --quiet code="code -n ."
+abbr -f --quiet g="git"
 
 # bundle
-abbr --quiet be="bundle exec "
-abbr --quiet bi="bundle install "
-abbr --quiet rails="bundle exec rails"
-abbr --quiet rails-s="bundle exec rails s -p 3001"
-abbr --quiet rspec="bundle exec rspec"
+abbr -f --quiet be="bundle exec "
+abbr -f --quiet bi="bundle install "
+abbr -f --quiet rails="bundle exec rails"
+abbr -f --quiet rails-s="bundle exec rails s -p 3001"
+abbr -f --quiet rspec="bundle exec rspec"
 # node
-abbr --quiet nir="ni run "
-abbr --quiet niu="ni upgrade-interactive "
-abbr --quiet nil="ni run lint "
-abbr --quiet sortp="npx sort-package-json"
+abbr -f --quiet nir="ni run "
+abbr -f --quiet niu="ni upgrade-interactive "
+abbr -f --quiet nil="ni run lint "
+abbr -f --quiet sortp="npx sort-package-json"
 # bun
-abbr --quiet bunup="bunx npm-check-updates -ui"
+abbr -f --quiet bunup="bunx npm-check-updates -ui"
 # docker
-abbr --quiet d="docker"
-abbr --quiet dc="docker-compose"
+abbr -f --quiet d="docker"
+abbr -f --quiet dc="docker-compose"
 
 case ${OSTYPE} in
 		darwin*)
@@ -46,7 +46,7 @@ case ${OSTYPE} in
 				;;
 		linux*)
 				#Linux用の設定
-				abbr --quiet open="xdg-open"
+				abbr -f --quiet open="xdg-open"
 				;;
 esac
 
@@ -54,18 +54,18 @@ ghq-get-cd() {
   ghq get "$1" && cd "$(ghq list --exact --full-path "$1")"
 }
 
-abbr --quiet "ghq get"="ghq-get-cd"
+abbr -f --quiet "ghq get"="ghq-get-cd"
 
-abbr --quiet "git sw"="git switch"
-abbr --quiet "git rs"="git restore"
-abbr --quiet "git cm"="git commit"
-abbr --quiet "git st"="git status"
-abbr --quiet "git br"="git branch"
-abbr --quiet "git lg"="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-abbr --quiet "git mt"="git mergetool"
-abbr --quiet "git dt"="git difftool"
-abbr --quiet "git pl"="git pull"
-abbr --quiet "git pp"="git pull --prune"
-abbr --quiet "git ps"="git push"
+abbr -f --quiet "git sw"="git switch"
+abbr -f --quiet "git rs"="git restore"
+abbr -f --quiet "git cm"="git commit"
+abbr -f --quiet "git st"="git status"
+abbr -f --quiet "git br"="git branch"
+abbr -f --quiet "git lg"="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+abbr -f --quiet "git mt"="git mergetool"
+abbr -f --quiet "git dt"="git difftool"
+abbr -f --quiet "git pl"="git pull"
+abbr -f --quiet "git pp"="git pull --prune"
+abbr -f --quiet "git ps"="git push"
 
-abbr --quiet "asdf neovim update"='asdf uninstall neovim nightly && asdf install neovim nightly'
+abbr -f --quiet "asdf neovim update"='asdf uninstall neovim nightly && asdf install neovim nightly'
