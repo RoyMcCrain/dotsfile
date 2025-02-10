88774c8aa9ba32adc129541335cbd95d0048442c
RoyMcCrain <me@rrrr.dev>
2025-02-10 19:24:39 +0900

    Update

---
+++ scripts/build_env/asdf_install_plugin.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+asdf install bat latest
+asdf global bat latest
+
+asdf install deno latest
+asdf global deno latest
+
+asdf install fzf latest
+asdf global fzf latest
+
+asdf install jq latest
+asdf global jq latest
+
+asdf install neovim latest
+asdf global neovim latest
+
+asdf install ripgrep latest
+asdf global ripgrep latest
+
+asdf install rust latest
+asdf global rust latest
+
+asdf install lua-language-server latest
+asdf global lua-language-server latest
