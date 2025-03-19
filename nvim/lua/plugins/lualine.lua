240e2517d246b35308517d2abb74e3b6381942ae
RoyMcCrain <me@rrrr.dev>
2025-03-20 00:49:11 +0900

    Add

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,61 @@
+local M = {}
+
+M.setup = function()
+  -- 総行数を表示するカスタム関数
+  local function line_count()
+    return tostring(vim.api.nvim_buf_line_count(0)) .. "L"
+  end
+  -- 文字数を表示するカスタム関数
+  local function char_count()
+    local wc = vim.fn.wordcount()
+    local mode = vim.fn.mode(true)
+
+    -- ビジュアルモードでの選択範囲の文字数
+    if mode:match('^[vV\22]') then
+      if wc.visual_chars then -- Neovim 0.7+
+        return wc.visual_chars .. "文字"
+      end
+    else
+      return ""
+    end
+  end
+
+  require('lualine').setup {
+    options = {
+      theme = 'material',
+      globalstatus = true,
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = {
+        'paste',
+        'branch',
+        'readonly',
+        'modified',
+      },
+      lualine_c = {
+        { 'diff', colored = true },
+        'diagnostics'
+      },
+      lualine_x = {
+        char_count,
+      },
+      lualine_y = {
+        'location',
+        line_count,
+        'fileformat',
+        'filetype',
+      },
+      lualine_z = {},
+    },
+    tabline = {
+      lualine_a = {},
+      lualine_b = { { 'filename', path = 1 } },
+      lualine_c = {},
+      lualine_y = { 'lsp_status' },
+      lualine_z = {},
+    },
+  }
+end
+
+return M
