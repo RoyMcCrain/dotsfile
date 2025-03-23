99555b497d9138f04c6f2f59bc4f39d0e9335069
RoyMcCrain <me@rrrr.dev>
2025-03-23 22:25:23 +0900

    Update zsh

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -1,7 +1,7 @@
 # Antigenの初期化
 source $HOME/.antigen/antigen.zsh
 
-antigen bundle zdharma-continuum/fast-syntax-highlighting@master
+antigen bundle zsh-users/zsh-syntax-highlighting@master
 antigen bundle zsh-users/zsh-completions@master
 antigen bundle zsh-users/zsh-autosuggestions@master
 antigen bundle azu/ni.zsh@main
@@ -9,37 +9,3 @@ antigen bundle olets/zsh-abbr@main
 
 # プラグインの適用
 antigen apply
-
-# fast-syntax-highlightingとzsh-abbrの連携
-chroma_single_word() {
-  (( next_word = 2 | 8192 ))
-
-  local __first_call="$1" __wrd="$2" __start_pos="$3" __end_pos="$4"
-  local __style
-
-  (( __first_call )) && { __style=${FAST_THEME_NAME}alias }
-  [[ -n "$__style" ]] && (( __start=__start_pos-${#PREBUFFER}, __end=__end_pos-${#PREBUFFER}, __start >= 0 )) && reply+=("$__start $__end ${FAST_HIGHLIGHT_STYLES[$__style]}")
-
-  (( this_word = next_word ))
-  _start_pos=$_end_pos
-
-  return 0
-}
-
-register_single_word_chroma() {
-  local word=$1
-  if [[ -x $(command -v $word) ]] || [[ -n $FAST_HIGHLIGHT["chroma-$word"] ]]; then
-    return 1
-  fi
-
-  FAST_HIGHLIGHT+=( "chroma-$word" chroma_single_word )
-  return 0
-}
-
-if [[ -n $FAST_HIGHLIGHT ]]; then
-  for abbr in ${(f)"$(abbr list-abbreviations)"}; do
-    if [[ $abbr != *' '* ]]; then
-      register_single_word_chroma ${(Q)abbr}
-    fi
-  done
-fi
